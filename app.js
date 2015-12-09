9c5171acb5357f97b4aadf8932af3d126b65f546
Jonatas Saraiva <jonatas.saraiva@gmail.com>
2015-12-09 21:01:59 -0200

    Alter send message from server

--- app.js
+++ app.js
@@ -3,8 +3,7 @@ var express = require('express');
 var app = module.exports = express();
 
 app.get('/', function (req, res) {
-	res.body = 'hello world';
-	res.send();
+	res.send('hello world');
 });
 
 var port = process.env.PORT || (process.argv[2] || 3000);
